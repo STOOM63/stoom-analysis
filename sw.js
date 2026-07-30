3862cc55af849b5d63b942a4d675b1a9c976b13d
STOOM63 <a.blanc63100@gmail.com>
2026-07-30 11:15:14 +0200

    Update service worker cache version and resources
    Updated cache version and added new CSS and JS files.

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE = 'analysis-v2.0.0';
+const CACHE = 'analysis-v2.0.1';
 const CORE = [
-  './','./index.html','./manifest.webmanifest','./assets/favicon.svg','./assets/css/styles.css',
-  './assets/js/app.js','./assets/js/core/utils.js','./assets/js/core/storage.js','./assets/js/core/importer.js','./assets/js/core/analytics.js','./assets/js/core/demo.js'
+  './','./index.html','./manifest.webmanifest','./assets/favicon.svg','./assets/css/styles.css','./assets/css/fixes.css',
+  './assets/js/ui-fixes.js','./assets/js/app.js','./assets/js/core/utils.js','./assets/js/core/storage.js','./assets/js/core/importer.js','./assets/js/core/analytics.js','./assets/js/core/demo.js'
 ];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
